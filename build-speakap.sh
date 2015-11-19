754eee2cf1560a9422f4b916828f5f19e8140a28
Arend van Beelen jr <arendjr@gmail.com>
2015-11-19 12:00:09 +0100

    No need for minified builds.

--- build-speakap.sh
+++ build-speakap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 bin/blade build
-cat dist/trix-core.js | sed 's/this\.Trix=/window.Trix=/' > dist/trix-core-commonjs.js
+cat dist/trix-core.js | sed 's/this\.Trix *=/window.Trix=/' > dist/trix-core-commonjs.js
